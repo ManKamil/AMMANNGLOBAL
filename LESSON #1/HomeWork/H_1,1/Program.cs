1c07cfef3bcbb2974651a75feb60077b4f7a0f9d
Kamil <slayercrew96@gmail.com>
2023-06-26 02:30:20 +0600

    add new hw #2

---
+++ LESSON #1/HomeWork/H_1,1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Please, insert first number");
+double num1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Please, insert second number");
+double num2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Please, insert third number");
+double num3 = double.Parse(Console.ReadLine()!);
+
+double max = num1;
+
+if (num1 > num2) max = num1;
+else {
+    max = num2;
+}
+
+if (max >= num3) max = num3;
+
+
+Console.WriteLine("Maximal number=");
+Console.WriteLine(max);
