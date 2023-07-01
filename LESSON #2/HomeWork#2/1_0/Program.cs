586efb7f838d3e0ae65bdf82416e9d6c916663c6
Kamil Manapov <Manapov.K@AB.int>
2023-07-01 13:46:40 +0600

    add new hw #1_0

--- LESSON #2/HomeWork#2/1_0/Program.cs
+++ LESSON #2/HomeWork#2/1_0/Program.cs
@@ -1,19 +1,11 @@
-﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-456 -> 5
-782 -> 8
-918 -> 1*/
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
 
-
-
-/*Console.Clear();
-Console.WriteLine("Введи трёхзначное число: ");
-string numA = Console.ReadLine();
-string numB = Console.ReadLine();
-string numC = Console.ReadLine();
-
-Console.Write("Второе число:  ");
-Console.Write(numB);*/
-
-
-Console.WriteLine ("Введите трёхзначное число:  ");
-
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
