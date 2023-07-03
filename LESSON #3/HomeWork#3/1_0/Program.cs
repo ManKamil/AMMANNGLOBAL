65e3541fa274d25946e27ede60e4a2d0f9fe8086
Kamil Manapov <Manapov.K@AB.int>
2023-07-03 15:38:39 +0600

    add new version HW 1_0

--- LESSON #3/HomeWork#3/1_0/Program.cs
+++ LESSON #3/HomeWork#3/1_0/Program.cs
@@ -3,9 +3,27 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Clear();
+/*Console.Clear();
 Console.WriteLine("Задача 10 ");
 Console.Write("Введи трёхзначное число: ");
 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);*/
+
+/*void numA(int num);
+{
+    if (num % 100 == 0) ;
+        
+}
+int num = int.Parse(Console.ReadLineLine());
+numA(num);*/
+
+Console.WriteLine("Please write a numbers: ");
+
+
+int numbers = int.Parse(Console.ReadLine()!);
+int result = 0;
+{
+   result = (numbers/10); 
+}
+Console.WriteLine(result%10);
