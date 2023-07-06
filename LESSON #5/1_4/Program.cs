e642980706879b718ae24a734538f4be6dd147b1
Kamil <slayercrew96@gmail.com>
2023-07-07 00:35:02 +0600

    add new class work lesson#5 1_4

--- LESSON #5/1_4/Program.cs
+++ LESSON #5/1_4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Найдите произведение пар чисел в одномерном массиве.
+//    Парой считаем первый и последний элемент, второй и предпоследний
+//    и т.д. Результат запишите в новом массиве.
+
+
+
+int[] NewArray(int[] arr)
+{
+    int Length = arr.Length;
+    int[] arr_2;
+
+    if (Length % 2 == 0) arr_2 = new int[Length / 2];
+    else arr_2 = new int[Length / 2 + 1];
+    for (int i = 0; i < arr_2.Length; i++)
+    {
+        if (arr[i] != arr[Length - i - 1]) arr_2[i] = arr[i] * arr[Length - i - 1];
+        else arr_2[arr_2.Length - 1] = arr[i];
+    }
+    return arr_2;
+
+    // int index = 0;
+    // while (A > 0)
+    // {
+    //     A /= 10;
+    //     index++;
+    // }
+    // return index;
+}
