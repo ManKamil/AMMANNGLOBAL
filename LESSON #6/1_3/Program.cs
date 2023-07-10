76165c5640c91f1085596d56033fde9fa22abee2
Kamil <slayercrew96@gmail.com>
2023-07-11 00:25:57 +0600

    add new class work 6 1_3

--- LESSON #6/1_3/Program.cs
+++ LESSON #6/1_3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//    Первые два числа Фибоначчи: 0 и 1.
+
+void Fibonachi(int N)
+{
+    int temp = 0;
+    int temp2 = 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        System.Console.WriteLine(temp);
+        (temp, temp2) = (temp2, temp + temp2);
+    }
+}
+
+Fibonachi(7);
