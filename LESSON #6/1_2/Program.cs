ed28b9e8b860634087855175dbdbc57f6571580d
Kamil <slayercrew96@gmail.com>
2023-07-11 00:16:57 +0600

    add new class work 6 1_2

--- LESSON #6/1_2/Program.cs
+++ LESSON #6/1_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+string BinaryNumber(int num)
+{
+    string number = string.Empty;
+
+    for (; num != 0; num /= 2)
+    {
+        number = num % 2 + number;
+    }
+    return number;
+}
+
+System.Console.WriteLine(BinaryNumber(12));
