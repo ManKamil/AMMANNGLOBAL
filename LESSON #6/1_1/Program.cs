96c36dbcf98a0bd98d985cba5f977105dbafddb6
Kamil <slayercrew96@gmail.com>
2023-07-11 00:06:23 +0600

    add new class work 6 1_1

--- LESSON #6/1_1/Program.cs
+++ LESSON #6/1_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+bool Triangle(int a, int b, int c)
+{
+    if (a < b + c && b < c + a && c < a + b)
+    return true;
+    else 
+    return false;
+}
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Triangle(a, b, c));
