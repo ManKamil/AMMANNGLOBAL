59bebb592ed9c532d8e706c45b6b3a330b93f3f5
Kamil Manapov <Manapov.K@AB.int>
2023-07-13 13:58:09 +0600

    add new hw 5lesson 1_0

---
+++ LESSON #5/HomeWork#5/1_0/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+void SumNum(int[] arr){
+    int Count = 0;
+    for(int i=0; i < arr.Length; i ++){
+        if(arr [i] % 2 ==0)
+            Count +=0;
+}
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = int.Parse(Console.ReadLine()!);
+int[]mass = MassNums(num1, num2, num3);
+Print(mass);
+
+System.Console.WriteLine(SumNum(mass));
