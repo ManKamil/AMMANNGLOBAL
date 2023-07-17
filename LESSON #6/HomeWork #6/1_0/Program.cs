c47a2dd6402777d0ff2674d0600e680a28d535b5
Kamil <slayercrew96@gmail.com>
2023-07-17 22:49:33 +0600

    HomeWork 6Lesson

---
+++ LESSON #6/HomeWork #6/1_0/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int Wirtg(int Num){
+    int count = 0;
+    for (i = 1; i <= num; i++){
+        Console.Write($"Число {i}: ");
+        int a = int.Parse(Console.ReadLine()!);
+        if (a>0) count ++;
+    }
+    return count;
+}
+Console.Write("Сколько чисел хотели бы ввести?: ");
+int a = int.Parse(Console.ReadLine()!);
+int resilt = checked(a);
+Console.WriteLine($"Кол-во чисел больше 0: {result}");
---
+++ LESSON #6/HomeWork #6/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+void ZeroZero(double k1, double b1, double k2, double b2)
+{
+        double k_num = k1 - k2;
+        if (k_num!=0)
+        {
+            double x = (b2-b1)/(k1-k2);
+            double y = k1 * x + b1;
+            Console.WriteLine($"point({x}; {y})");
+        }
+    else if (k2 == k2 && b2 == b2)
+        Console.WriteLine("There are an an infinite number of intersection points. ");
+    else
+        Console.WriteLine("There is no intersection point");
+}
