453e25da23bb3a8230bacd4f6643a45e7ea31d21
Kamil Manapov <Manapov.K@AB.int>
2023-07-19 15:00:20 +0600

    add new home work lesson#7 1_0

--- LESSON #5/HomeWork#5/1_0/Program.cs
+++ LESSON #5/HomeWork#5/1_0/Program.cs
@@ -41,3 +41,32 @@ int[]mass = MassNums(num, start, stop);
 Print(mass);
 Console.WriteLine(RanNums(Mass));
 
+
+
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+/*Console.Write("Введите количество элементов массива: ");
+int a = int.Parse(Console.ReadLine()!);
+int[] randomArray = new int[a];
+
+void mass(int a)
+{
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(99,999);
+Console.Write(randomArray[i] + " " );
+}
+
+}
+
+int kolichestvo(int[] randomArray)
+{
+int kol = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+kol = kol + 1;
+}
+return kol;
+}*/
